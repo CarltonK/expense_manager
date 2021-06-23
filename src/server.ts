865e42a696d4ce487e4bfa3d465aa7d9918e6671
Mark Carlton <mcarlton33@gmail.com>
2021-06-23 12:41:23 +0300

    connect to db

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import App from './app';
-// import DBConfig from './config/db';
+import DBConfig from './config/db';
 import BaseController from './controllers/base';
 
 const port = process.env.PORT || 8080;
 
-// new DBConfig();
+new DBConfig();
 
 const app = new App(
   [
