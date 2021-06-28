1bc7380a2811b865f943c23ad31bf87c08adc84c
Mark Carlton <mcarlton33@gmail.com>
2021-06-28 09:24:59 +0300

    add new controllers

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import App from './app';
 import DBConfig from './config/db';
 import BaseController from './controllers/base';
+import SessionController from './controllers/session';
+import UserController from './controllers/user';
 
 const port = process.env.PORT || 8080;
 
@@ -9,6 +11,8 @@ new DBConfig();
 const app = new App(
   [
     new BaseController(),
+    new SessionController(),
+    new UserController(),
   ],
   port,
 );
