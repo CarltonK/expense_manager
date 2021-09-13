4aa4182f9ace1299c249563979b4d45744d61dff
Mark Carlton <mcarlton33@gmail.com>
2021-09-13 21:08:17 +0300

    prisma integration

--- docker/prod.Dockerfile
+++ docker/prod.Dockerfile
@@ -1,5 +1,7 @@
 FROM node:12-alpine
 
+ARG DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+
 WORKDIR /app
 
 # add `/app/node_modules/.bin` to $PATH
@@ -13,6 +15,12 @@ RUN npm i
 COPY . /app
 
 RUN npm run lint
+
+RUN sed -i 's/#{DB_USER}#/$DB_USER/g' .env
+RUN sed -i 's/#{DB_PASS}#/$DB_PASS/g' .env
+RUN sed -i 's/#{DB_HOST}#/$DB_HOST/g' .env
+RUN sed -i 's/#{DB_PORT}#/$DB_PORT/g' .env
+RUN sed -i 's/#{DB_NAME}#/$DB_NAME/g' .env
 
 RUN npm run build
 
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,25 @@
 import { PrismaClient } from "@prisma/client";
+import { exec } from "child_process"
 
 export default class DBConfig {
     public prisma: PrismaClient;
     constructor() {
         this.prisma = new PrismaClient();
+        this.runMigrations().catch(error => console.error(error));
+    }
+
+    async runMigrations() {
+        return new Promise((resolve, reject) => {
+            exec('npx prisma migrate dev --name init', { maxBuffer: 1024 * 500 }, (error, stdout, stderr) => {
+                if (error) {
+                    console.warn(error);
+                } else if (stdout) {
+                    console.log(stdout);
+                } else {
+                    console.log(stderr);
+                }
+                resolve(stdout ? true : false);
+            });
+        });
     }
 }
