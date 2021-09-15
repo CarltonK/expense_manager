6ad27aa49d63a2f1a6a5c1f44e23490d5d6b9092
Mark Carlton <mcarlton33@gmail.com>
2021-09-15 07:14:47 +0300

    removed need to run migrations

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,14 +3,14 @@ import { exec } from "child_process"
 
 export default class DBConfig {
     public prisma: PrismaClient;
+
     constructor() {
         this.prisma = new PrismaClient();
-        this.runMigrations().catch(error => console.error(error));
     }
 
     async runMigrations() {
         return new Promise((resolve, reject) => {
-            exec('npx prisma migrate dev --name init', { maxBuffer: 1024 * 500 }, (error, stdout, stderr) => {
+            exec('npm run migrations:prod', { maxBuffer: 1024 * 500 }, (error, stdout, stderr) => {
                 if (error) {
                     console.warn(error);
                 } else if (stdout) {
